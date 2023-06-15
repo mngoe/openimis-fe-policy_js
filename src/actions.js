5d8e3cbf3e0f0d17fe7ff9011de05cb3bce03465
jobil <j.bilongo@ynote.africa>
2023-06-15 19:29:24 +0100

    last changes

--- src/actions.js
+++ src/actions.js
@@ -9,7 +9,7 @@ import {
 import _ from "lodash";
 import { decodeId } from "@openimis/fe-core";
 
-const FAMILY_HEAD_PROJECTION = "headInsuree{id,uuid,chfId,lastName,otherNames,email,phone,dob,gender{code}}";
+const FAMILY_HEAD_PROJECTION = "headInsuree{id,uuid,chfId,lastName,otherNames,email, passport,phone,dob,gender{code}}";
 
 const POLICY_BY_FAMILY_OR_INSUREE_PROJECTION = [
   "policyUuid",
@@ -47,23 +47,6 @@ const FAMILY_FULL_PROJECTION = (mm) => [
   "clientMutationId",
 ];
 
-const FAMILY_HEAD_PROJECTION = "headInsuree{id,uuid,chfId,lastName,otherNames,email,phone,dob,gender{code}}";
-
-const FAMILY_FULL_PROJECTION = (mm) => [
-  "id",
-  "uuid",
-  "poverty",
-  "confirmationNo",
-  "confirmationType{code}",
-  "familyType{code}",
-  "address",
-  "validityFrom",
-  "validityTo",
-  FAMILY_HEAD_PROJECTION,
-  "location" + mm.getProjection("location.Location.FlatProjection"),
-  "clientMutationId",
-];
-
 export function fetchFamilyOrInsureePolicies(mm, filters) {
   let qry = "policiesByFamily";
   let RDX = "POLICY_FAMILY_POLICIES";
@@ -78,17 +61,6 @@ export function fetchFamilyOrInsureePolicies(mm, filters) {
   );
   return graphql(payload, RDX);
 }
-
-export function fetchFamily(mm, familyUuid, headInsureeChfId) {
-  let filters = [];
-  if (!!familyUuid) {
-    filters.push(`uuid: "${familyUuid}"`, "showHistory: true");
-  } else {
-    filters.push(`headInsuree_ChfId: "${headInsureeChfId}"`);
-  }
-  const payload = formatPageQuery("families", filters, FAMILY_FULL_PROJECTION(mm));
-  return graphql(payload, "INSUREE_FAMILY_OVERVIEW");
-};
 
 export function fetchEligibility(chfid) {
   let payload = `
