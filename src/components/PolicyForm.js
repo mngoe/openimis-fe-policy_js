822f60f70a191c8bc6b6d8bcd2b49df67d9e0972
kouamkouammichellewis <kouamkouammichellewis@gmail.com>
2023-10-31 09:16:27 +0100

    add is notExist

--- src/components/PolicyForm.js
+++ src/components/PolicyForm.js
@@ -157,27 +157,27 @@ class PolicyForm extends Component {
   }
 
   canSave = () => {
-    if (!this.state.policy.family) return false;
-    if (!this.state.policy.product) return false;
+    // if (!this.state.policy.family) return false;
+    // if (!this.state.policy.product) return false;
 
-    //check if vih insuree have vih policy
-    if (this.state.policy.family.headInsuree.email == "newhivuser_XM7dw70J0M3N@gmail.com") {
-      if (this.state.policy.product.program.nameProgram != "VIH") return false;
-    } else {
-      if (this.state.policy.product.program.nameProgram == "VIH") return false;
-    }
+    // //check if vih insuree have vih policy
+    // if (this.state.policy.family.headInsuree.email == "newhivuser_XM7dw70J0M3N@gmail.com") {
+    //   if (this.state.policy.product.program.nameProgram != "VIH") return false;
+    // } else {
+    //   if (this.state.policy.product.program.nameProgram == "VIH") return false;
+    // }
 
-    //check policy number if is cs product
-    if ((this.state.policy.product.program.nameProgram) == "Cheque Santé") {
-      if (!this.state.policy.policyNumber) return false;
-      if(this.state.policy.policyNumber.chequeImportLineStatus === "used") return false;
-    }
+    // //check policy number if is cs product
+    // if ((this.state.policy.product.program.nameProgram) == "Cheque Santé") {
+    //   if (!this.state.policy.policyNumber) return false;
+    //   if(this.state.policy.policyNumber.chequeImportLineStatus === "used") return false;
+    // }
 
-    if (!this.state.policy.enrollDate) return false;
-    if (!this.state.policy.startDate) return false;
-    if (!this.state.policy.expiryDate) return false;
-    //if (!this.state.policy.value) return false;
-    if (!this.state.policy.officer) return false;
+    // if (!this.state.policy.enrollDate) return false;
+    // if (!this.state.policy.startDate) return false;
+    // if (!this.state.policy.expiryDate) return false;
+    // //if (!this.state.policy.value) return false;
+    // if (!this.state.policy.officer) return false;
     return true;
   }
 
