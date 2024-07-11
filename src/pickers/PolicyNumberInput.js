c9fb5c8e3610008de85aa6137aaae328d46a32f7
michel-lewis <kouamkouammichellewis@gmail.com>
2024-07-11 12:50:22 +0100

    fix: policy form cheque status

--- src/pickers/PolicyNumberInput.js
+++ src/pickers/PolicyNumberInput.js
@@ -64,7 +64,8 @@ class PolicyNumberInput extends Component {
 
   render() {
     const { intl, readOnly, required, error, policyNumber, fetching, withLabel, label} = this.props;
-    const isInvalid = !fetching && policyNumber && policyNumber.chequeImportLineStatus === "used" || !fetching && policyNumber === undefined;
+    const isInvalid = !fetching && policyNumber && (policyNumber.chequeImportLineStatus).toLowerCase() === "used" || !fetching && policyNumber === undefined;
+    console.log("isinvalid ", isInvalid)
     const isNotExit = !fetching && policyNumber === undefined;
     return (
       <TextInput
