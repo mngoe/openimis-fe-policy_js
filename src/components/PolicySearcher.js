4bc8e5438ceb974af3cc2ce95622498883163efe
vaneck237 <vaneckbilongo@gmail.com>
2024-12-15 19:37:20 +0100

    fix conflicts

--- src/components/PolicySearcher.js
+++ src/components/PolicySearcher.js
@@ -189,8 +189,6 @@ class PolicySearcher extends Component {
                 this.props.modulesManager,
                 this.props.intl,
                 policy.validityTo),
-<<<<<<< HEAD
-=======
             filters?.showHistory?.value
               ? (policy) =>
                   formatDateFromISO(
@@ -207,7 +205,6 @@ class PolicySearcher extends Component {
                     policy.validityTo
                   )
               : null,
->>>>>>> 494c18713e02130c84178e18a742e93284b32c23
             policy => {
                 if (!policy.family) return null
                 return (
@@ -279,10 +276,6 @@ class PolicySearcher extends Component {
                     rowLocked={this.rowLocked}
                     onDoubleClick={(i) => !i.clientMutationId && onDoubleClick(i)}
                     canFetch={false}
-<<<<<<< HEAD
-=======
-
->>>>>>> 494c18713e02130c84178e18a742e93284b32c23
                 />
             </Fragment>
         )
