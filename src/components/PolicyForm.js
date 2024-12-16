03b11534cf6e1794435f498223324190d90d1131
vaneck237 <vaneckbilongo@gmail.com>
2024-12-16 10:25:13 +0100

    add console log

--- src/components/PolicyForm.js
+++ src/components/PolicyForm.js
@@ -290,6 +290,7 @@ class PolicyForm extends Component {
 
     //check policy number if is cs product
     if ((this.state.policy.product.program.nameProgram) == "Chèque Santé" || (this.state.policy.product.program.nameProgram) == "Cheque Santé") {
+      console.log(this.state.policy.policyNumber);
       if (!this.state.policy.policyNumber) return false;
       if (this.state.policy.policyNumber.chequeImportLineStatus === "used") return false;
       if ((this.state.policy.policyNumber.chequeImportLineStatus).toLowerCase() === "used") return false;
